f6cd355525b26465bbe99eb3b269f20a78de3855
AungHtet21 <aunghtetn1997@gmail.com>
2025-04-21 15:06:59 +0630

    21-April-2025

--- project_files/5.optimal.sql
+++ project_files/5.optimal.sql
@@ -1,22 +1,20 @@
-WITH skill_name AS (
-    SELECT skills_job_dim.skill_id,
-        skills_dim.skills AS skill,
-        skills_job_dim.job_id,
-        job_posting_fact.salary_year_avg,
-        job_posting_fact.job_title_short,
-        job_posting_fact.job_location
+WITH skill_name AS(
+    SELECT skills_job_dim.*,
+        skills_dim.skills,
+        job_posting_fact.salary_year_avg
     FROM skills_job_dim
         LEFT JOIN skills_dim ON skills_dim.skill_id = skills_job_dim.skill_id
         LEFT JOIN job_posting_fact ON job_posting_fact.job_id = skills_job_dim.job_id
-    WHERE job_posting_fact.job_title_short = 'Data Analyst'
-        AND job_posting_fact.job_location = 'Anywhere'
+    WHERE job_title_short = 'Data Analyst'
+        AND job_location = 'Anywhere'
+        AND salary_year_avg IS NOT NULL
 )
-SELECT skill_name.skill_id,
-    skill_name.skill AS skills,
-    COUNT(skill_name.job_id) AS demand_count,
-    ROUND(AVG(skill_name.salary_year_avg), 2) AS average_salary_year
+SELECT skill_id,
+    skills,
+    COUNT(skill_name.job_id) AS Demand_count,
+    ROUND(AVG(salary_year_avg), 2) AS Average_Salary_Year
 FROM skill_name
-WHERE salary_year_avg IS NOT NULL
-GROUP BY skill_name.skill_id,
-    skill_name.skill
-ORDER BY demand_count DESC;
+GROUP BY (skill_id, skills)
+ORDER BY Demand_count DESC,
+    Average_Salary_Year DESC
+LIMIT 25;
